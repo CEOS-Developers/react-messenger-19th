a2f0ef0276cb131265258b9a35bf1e76cffeb618
Programming-Seungwan <swsj4480@gmail.com>
2024-03-26 00:49:53 +0900

    Feat : styled-components를 통해 figma의 디자인 시스템 구축
    1. ThemeProvider 컴포넌트 이용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+
 function App() {
-	return (
-		<div>
-			<h1>19기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
-		</div>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>19기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,35 @@
+const theme = {
+  color: {
+    key: '#5865F2',
+    white: '#FFFFFF',
+    grayLight: '#F2F3F5',
+    grayMedium: '#EBEBEB',
+    grayDark: '#4E5058',
+    black: '#060607',
+  },
+  textStyle: {
+    fontSize: {
+      h1: '32',
+      h2: '20',
+      h3: '16',
+      body1: '15',
+      body2: '15',
+      label: '13',
+      caption: '10',
+    },
+    lineHeight: {
+      h1: '150',
+      h2: '150',
+      h3: '150',
+      body1: '150',
+      body2: '150',
+      label: '150',
+      caption: '150',
+    },
+  },
+  gridStyle: {
+    display: 'grid',
+  },
+};
+
+export default theme;
