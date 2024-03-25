f2e5ee27e48164b1feb14f4b2660467bcfd9b347
Programming-Seungwan <swsj4480@gmail.com>
2024-03-26 00:58:43 +0900

    Chore : apply reset.css
    1. styled-reset 모듈을 통해 컴포넌트 방식으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import GlobalStyles from './styles/globalStyles';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <h1>19기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
-      </div>
-    </ThemeProvider>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <div>
+          <h1>19기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
