ec862534e6af82c7a18738a0430fee9709dea8f6
Programming-Seungwan <swsj4480@gmail.com>
2024-03-26 02:02:50 +0900

    Fix : craco runner를 통한 ts 상대경로 webpack 문제 해결
    1. 기존의 react-scripts 러너는 상대 경로를 설정해주더라도 알아서 이를 씹고 CRA의 기본 설정으로 경로를 인식하여 오류가 생김
    2. tsconfig.paths.json 파일에 기존의 설정을 해주고 @craco/craco 모듈과 react-app-alias 모듈을 설치
    3. craco.config.json 파일에 craco 관련 설정을 해줌
    4. 애플리케이션 러너 환경 자체를 react-scripts가 아닌, craco 가 돌릴 수 있게 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/globalStyles';
+import Test from '@components/test/Test.tsx';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <div>
-          <h1>19기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
-        </div>
+        <Test />
       </ThemeProvider>
     </>
   );
---
+++ src/components/test/Test.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledTestDiv = styled.div``;
+
+export default function Test() {
+  return <StyledTestDiv>This is test div!!</StyledTestDiv>;
+}
