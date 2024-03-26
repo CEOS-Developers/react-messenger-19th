147aa94568529e77d85cc27bd78648b5fbf4a305
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-27 00:53:18 +0900

    [init] 아이콘 초기세팅

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import BackArrow from "./icon/backArrow.svg?react";
+import Call from "./icon/call.svg?react";
+import Emoji from "./icon/emoji.svg?react";
+import Find from "./icon/find.svg?react";
+import Index from "./icon/index.svg?react";
+import Info from "./icon/info.svg?react";
+import Plus from "./icon/plus.svg?react";
+import Profile from "./icon/profile.svg?react";
+import Voice from "./icon/voice.svg?react";
+
+export { BackArrow, Call, Emoji, Find, Index, Info, Plus, Profile, Voice };
