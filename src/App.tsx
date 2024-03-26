788a801e97aa13aefd1f1689373f31907fbb3399
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-27 02:05:37 +0900

    [init] 프리티어

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import GlobalStyle from "./styles/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-import Router from "./Router";
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import Router from './Router';
 
 export function App() {
   return (
