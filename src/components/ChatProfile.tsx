c25bdbf58a5c073b1109cc0adc3e1345a0c27c71
Dahn12 <dadesign0709@gmail.com>
2024-03-26 19:41:14 +0900

    feat: header 시간표시

---
+++ src/components/ChatProfile.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+function ChatProfile() {
+  return (
+    <ChatProfileWrapper>ChatProfile</ChatProfileWrapper>
+  )
+}
+
+const ChatProfileWrapper = styled.div`
+    background-color: beige;
+    width: 100%;
+    height: 155.21px;
+`
+
+export default ChatProfile
--- src/pages/Chat/ChattingPage.tsx
+++ src/pages/Chat/ChattingPage.tsx
@@ -1,14 +1,21 @@
 import styled from "styled-components"
+import Header from "../../components/Header"
+import ChatProfile from "../../components/ChatProfile"
 
 function ChattingPage() {
   return (
-    <Wrapper>ChattingPage</Wrapper>
+    <Wrapper>
+       <Header/> 
+       <ChatProfile/>
+    </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
 export default ChattingPage
