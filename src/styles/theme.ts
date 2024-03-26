5f1c4fc2e9d96845dd323477a330a668f558f776
eunsu <songess@naver.com>
2024-03-26 14:30:04 +0900

    style: globalStyle 및 theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,88 @@
+const colors = {
+  black: '#121212',
+  white: '#FFFFFF',
+  orange: '#FF6128',
+  yellow: '#FEE500',
+  blue: '#ABC1D1',
+  gray_1: '#5C666E',
+  gray_2: '#828282',
+  gray_3: '#A7ACAF',
+  gray_4: '#E0E0E0',
+  gray_5: '#F5F5F5',
+  gray_6: '#F9F9FA',
+};
+
+const textStyles = {
+  title: {
+    font_size: '22px',
+    line_height: '32px',
+    font_weight: '600',
+  },
+  subTitle: {
+    font_size: '18px',
+    line_height: '28px',
+    font_weight: '600',
+  },
+  body1_1: {
+    font_size: '14px',
+    line_height: '16px',
+    font_weight: '400',
+  },
+  body1_2: {
+    font_size: '14px',
+    line_height: '16px',
+    font_weight: '500',
+  },
+  body2_1: {
+    font_size: '15px',
+    line_height: '20px',
+    font_weight: '500',
+  },
+  body2_2: {
+    font_size: '15px',
+    line_height: '24px',
+    font_weight: '600',
+  },
+  body3: {
+    font_size: '16px',
+    line_height: '24px',
+    font_weight: '400',
+  },
+  body4: {
+    font_size: '18px',
+    line_height: '28px',
+    font_weight: '500',
+  },
+  caption1: {
+    font_size: '8px',
+    line_height: '12px',
+    font_weight: '600',
+  },
+  caption2: {
+    font_size: '9px',
+    line_height: '12px',
+    font_weight: '400',
+  },
+  caption3: {
+    font_size: '10px',
+    line_height: '16px',
+    font_weight: '500',
+  },
+  caption4: {
+    font_size: '11px',
+    line_height: '16px',
+    font_weight: '400',
+  },
+  caption5: {
+    font_size: '12px',
+    line_height: '16px',
+    font_weight: '500',
+  },
+};
+
+const theme = {
+  colors,
+  textStyles,
+};
+
+export default theme;
