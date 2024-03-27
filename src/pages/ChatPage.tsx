cb118977d7acfd754ecacdc4f97cc764f6bd5a2c
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-27 17:50:12 +0900

    [feat] ChatPage 컴포넌트 추가

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -2,12 +2,14 @@ import Header from '../components/@common/Header';
 import ChatHeader from '../components/chatPage/ChatHeader';
 import ChatFooter from '../components/chatPage/ChatFooter';
 import Footer from '../components/@common/Footer';
+import ChatContainer from '../components/chatPage/ChatContainer';
 
 export default function ChatPage() {
   return (
     <>
       <Header />
       <ChatHeader />
+      <ChatContainer />
       <ChatFooter />
       <Footer />
     </>
