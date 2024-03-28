aba84cc045623a35f5fa4261d561e15a77d1fa64
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-03-28 23:46:24 +0900

    feat: ChatHead

--- src/components/ChatHead.tsx
+++ src/components/ChatHead.tsx
@@ -10,8 +10,17 @@ interface ChatHeadProps {
 	onUserClick: () => void;
 }
 
-const ChatHead: React.FC<ChatHeadProps> = () => {
-	return <div></div>;
+const ChatHead: React.FC<ChatHeadProps> = ({ user, onUserClick }) => {
+	return (
+		<div onClick={onUserClick}>
+			<img
+				src={user.image}
+				alt='Profile'
+				style={{ width: '50px', height: '50px', borderRadius: '50%' }}
+			/>
+			<h2>{user.name}</h2>
+		</div>
+	);
 };
 
 export default ChatHead;
