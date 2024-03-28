361122581d263058b024b51c17be80d818cde96a
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-03-28 23:43:31 +0900

    feat: ChatBody (messages, currentUser)

--- src/components/ChatBody.tsx
+++ src/components/ChatBody.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 
 interface Message {
-  id: number;
-  from: string;
-  content: string;
-  date: string;
+	id: number;
+	from: string;
+	content: string;
+	date: string;
 }
 
 interface ChatBodyProps {
-  messages: Message[];
-  currentUser: string;
+	messages: Message[];
+	currentUser: string;
 }
 
+const ChatBody: React.FC<ChatBodyProps> = ({ messages, currentUser }) => {
+	return (
+		<div>
+			{messages.map((message) => (
+				<div key={message.id}>
+					<div>{message.from}</div>
+					<div>{message.content}</div>
+					<div>{message.date}</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
-
-const ChatBody: React.FC<ChatBodyProps> = () => {
-    return (
-      <div>
-      </div>
-    );
-  };
-  
-  export default ChatBody;
-  
+export default ChatBody;
