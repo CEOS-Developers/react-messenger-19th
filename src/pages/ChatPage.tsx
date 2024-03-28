5d5b99563580ca5e9c552e184529dcec4f4c507b
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-28 15:25:19 +0900

    [feat] ChatPage 타입 fix

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -4,16 +4,16 @@ import ChatFooter from '../components/chatPage/ChatFooter';
 import Footer from '../components/@common/Footer';
 import ChatContainer from '../components/chatPage/ChatContainer';
 import { useState } from 'react';
-import { MessageTypes } from '../components/chatPage/ChatFooter';
+import { MsgType } from '../types/types';
 
 export default function ChatPage() {
-  const [list, setList] = useState<MessageTypes[]>([]);
+  const [list, setList] = useState<MsgType[]>([]);
 
   return (
     <>
       <Header />
       <ChatHeader />
-      <ChatContainer list={list} setList={setList} />
+      <ChatContainer list={list} />
       <ChatFooter list={list} setList={setList} />
       <Footer />
     </>
