a70527e6f43fd4cbeca777e51ad6139e99ed591c
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-29 19:58:45 +0900

    [init] recoil 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import Router from './Router';
+import { RecoilRoot } from 'recoil';
 
 export function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </RecoilRoot>
     </>
   );
 }
