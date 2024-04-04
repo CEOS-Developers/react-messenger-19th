52c9275d162c2ad9f389284b175444284570b5d8
Rose <120544840+Rose-my@users.noreply.github.com>
2024-04-04 14:43:38 +0900

    [ feat ] 라우터 세팅 outlet, query 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ChatPage from './pages/ChatPage';
+import Layout from './pages';
+import FriendsPage from './pages/FriendsPage';
+import ChatsPage from './pages/ChatsPage';
+import ChatsDetailPage from './pages/ChatsDetailPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ChatPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/friends" element={<FriendsPage />} />
+          <Route path="/chats" element={<ChatsPage />}>
+            <Route path=":userId" element={<ChatsDetailPage />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ChatsDetailPage.tsx
+++ src/pages/ChatsDetailPage.tsx
@@ -1,22 +1,18 @@
-import Header from '../components/@common/Header';
 import ChatHeader from '../components/chatPage/ChatHeader';
 import ChatFooter from '../components/chatPage/ChatFooter';
-import Footer from '../components/@common/Footer';
 import ChatContainer from '../components/chatPage/ChatContainer';
 import { useState } from 'react';
 import { MsgType } from '../types/types';
 
-export default function ChatPage() {
+export default function ChatsDetailPage() {
   const [list, setList] = useState<MsgType[]>([]);
   const [userId, setUserId] = useState(0);
 
   return (
     <>
-      <Header />
       <ChatHeader userId={userId} setUserId={setUserId} />
       <ChatContainer list={list} userId={userId} />
       <ChatFooter list={list} setList={setList} />
-      <Footer />
     </>
   );
 }
