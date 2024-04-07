fb88fff1d432cb1922b53b2599916071d153231b
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-04-07 16:26:05 +0900

    feat: Footer 하단 절대위치 지정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Footer from '../components/common/Footer';
 import FooterMenu from '../components/common/FooterMenu';
 import Header from '../components/common/Header';
@@ -5,11 +6,19 @@ import UserList from '../components/units/Main/UserList';
 
 export default function Main(): JSX.Element {
 	return (
-		<div>
+		<div style={{ position: 'relative', height: '100%' }}>
 			<Header color="white" />
 			<UserList />
-			<FooterMenu />
-			<Footer />
+			<MainFooter>
+				<FooterMenu />
+				<Footer />
+			</MainFooter>
 		</div>
 	);
 }
+
+const MainFooter = styled.div`
+	width: 100%;
+	position: absolute;
+	bottom: 0;
+`;
