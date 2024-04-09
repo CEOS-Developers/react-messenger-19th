44e965f571ad6121fa8be4c6f5aeb393d4963d02
Rose <120544840+Rose-my@users.noreply.github.com>
2024-04-09 11:07:02 +0900

    [ etc ] 파일 경로 수정

--- src/pages/ChatsDetailPage.tsx
+++ src/pages/ChatsDetailPage.tsx
@@ -1,18 +1,14 @@
-import ChatHeader from '../components/chatPage/ChatHeader';
-import ChatFooter from '../components/chatPage/ChatFooter';
-import ChatContainer from '../components/chatPage/ChatContainer';
-import { useState } from 'react';
-import { MsgType } from '../types/types';
+import Header from '../components/chatsDetailPage/Header';
+import Footer from '../components/chatsDetailPage/Footer';
+import BoxLayout from '../components/chatsDetailPage/BoxLayout';
 
 export default function ChatsDetailPage() {
-  const [list, setList] = useState<MsgType[]>([]);
-  const [userId, setUserId] = useState(0);
 
   return (
     <>
-      <ChatHeader userId={userId} setUserId={setUserId} />
-      <ChatContainer list={list} userId={userId} />
-      <ChatFooter list={list} setList={setList} />
+      <Header />
+      <BoxLayout />
+      <Footer />
     </>
   );
 }
