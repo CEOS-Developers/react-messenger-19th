a4033f7e586339174a9c44cea37ed3f6da39e8a7
Rose <120544840+Rose-my@users.noreply.github.com>
2024-04-11 01:15:04 +0900

    [ feat ] 마지막 채팅, 날짜 불러오기

--- src/components/chatsPage/ChatsLayout.tsx
+++ src/components/chatsPage/ChatsLayout.tsx
@@ -1,15 +1,29 @@
 import styled from 'styled-components';
 import userData from '../../assets/data/userData.json';
+import chatData from '../../assets/data/chatData.json';
 import ChatsBox from './ChatsBox';
+import { chatsState } from '../../recoil/atom';
+import { useRecoilValue } from 'recoil';
 
 export default function ChatsLayout() {
+  const chats = useRecoilValue(chatsState);
+
   return (
     <Wrapper>
       {userData.data
         .filter((user) => user.id >= 1)
         .map((user) => {
           const { id, name } = user;
-          return <ChatsBox key={id} id={id} name={name} text={'안녕 못한다 이자식아'} time={'오전 10:06'} />;
+
+          const filteredChats = [...chatData.data, ...chats].filter((chat) => {
+            return (chat.from === 0 && chat.to === id) || (chat.to === 0 && chat.from === id);
+          });
+
+          const lastChat = filteredChats.length > 0 ? filteredChats[filteredChats.length - 1] : null;
+          const text = lastChat ? lastChat.details.text : '';
+          const time = lastChat ? lastChat.details.time : '';
+
+          return <ChatsBox key={id} id={id} name={name} text={text} time={time} />;
         })}
     </Wrapper>
   );
