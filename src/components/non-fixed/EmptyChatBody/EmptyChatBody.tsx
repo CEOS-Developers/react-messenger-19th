562c30c2728210595d3040f0d560c805d62cf1ba
Programming-Seungwan <swsj4480@gmail.com>
2024-04-25 23:11:18 +0900

    Feat : make empty chat default UI
    1. 기존의 메시지 데이터가 있는 상황에 대해서 상태관리 설정을 추후에 해줘야 함

--- src/components/non-fixed/EmptyChatBody/EmptyChatBody.tsx
+++ src/components/non-fixed/EmptyChatBody/EmptyChatBody.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import ProfileGroupUI from '@components/non-fixed/EmptyChatBody/ProfileGroupUI/ProfileGroupUI';
+import { useEffect } from 'react';
 
 const StyledEmptyChatBody = styled.div`
   flex-grow: 1;
@@ -7,8 +9,31 @@ const StyledEmptyChatBody = styled.div`
   margin-left: 16px;
   width: 343px;
   overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
 `;
 
-export default function EmptyChatBody() {
-  return <StyledEmptyChatBody className="scroll-box"></StyledEmptyChatBody>;
+const StyledProfileGroupUIContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+  /* padding: 16px 94px 0px 94px; */
+  padding-top: 16px;
+  display: flex;
+  justify-content: center;
+`;
+
+export default function EmptyChatBody({
+  username,
+}: {
+  username: string | undefined;
+}) {
+  // 기존의 데이터가 없으면 로고를 보여주고, 아니면 이에 맞는 메시지 데이터들을 보여주면 된다
+
+  return (
+    <StyledEmptyChatBody className="scroll-box">
+      <StyledProfileGroupUIContainer>
+        <ProfileGroupUI username={username} />
+      </StyledProfileGroupUIContainer>
+    </StyledEmptyChatBody>
+  );
 }
--- src/pages/EmptyChat.tsx
+++ src/pages/EmptyChat.tsx
@@ -5,11 +5,11 @@ import EmptyChatBody from '@components/non-fixed/EmptyChatBody/EmptyChatBody';
 
 export default function EmptyChat() {
   const { username } = useParams();
-  console.log(username);
+
   return (
     <>
       <EmptyChatHeadNav username={username} />
-      <EmptyChatBody />
+      <EmptyChatBody username={username} />
       <ChatInput />
     </>
   );
