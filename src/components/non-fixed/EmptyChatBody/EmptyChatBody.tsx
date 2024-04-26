53fa57a26ef4440f00fe3151f26a9b344828b074
Programming-Seungwan <swsj4480@gmail.com>
2024-04-27 00:34:31 +0900

    Chore : messageData가 없을 때 ProfileGroup UI로 보이게 조건부 렌더링

--- src/components/non-fixed/EmptyChatBody/EmptyChatBody.tsx
+++ src/components/non-fixed/EmptyChatBody/EmptyChatBody.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import ProfileGroupUI from '@components/non-fixed/EmptyChatBody/ProfileGroupUI/ProfileGroupUI';
 import { useEffect } from 'react';
+import { messageDataObject } from 'types/type';
+import { useRecoilState } from 'recoil';
+import { messageDataState, messageDateArrayState } from '@context/state/atom';
+import ldsh from 'lodash'; // 객체의 깊은 비교를 위한 라이브러리에 해당
 
 const StyledEmptyChatBody = styled.div`
   flex-grow: 1;
@@ -10,7 +14,7 @@ const StyledEmptyChatBody = styled.div`
   width: 343px;
   overflow-y: scroll;
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
 `;
 
 const StyledProfileGroupUIContainer = styled.div`
@@ -27,13 +31,44 @@ export default function EmptyChatBody({
 }: {
   username: string | undefined;
 }) {
+  const [messageData, setMessageData] = useRecoilState(messageDataState);
+  const [messageDateArray, setMessageDateArray] = useRecoilState(
+    messageDateArrayState
+  );
+
   // 기존의 데이터가 없으면 로고를 보여주고, 아니면 이에 맞는 메시지 데이터들을 보여주면 된다
+
+  useEffect(() => {
+    if (
+      localStorage.getItem(`chatMessageData${username}`) === null &&
+      localStorage.getItem(`chatMessageDateArray${username}`) === null
+    ) {
+      setMessageData({});
+      setMessageDateArray([]);
+    } else if (
+      // 데이터가 있는 경우 -> 상태를 변경시키고 이를 이용하여 리렌더링 진행
+      localStorage.getItem(`chatMessageData${username}`) !== null &&
+      localStorage.getItem(`chatMessageDateArray${username}`) !== null
+    ) {
+      const lstrgChatMessageData = JSON.parse(
+        localStorage.getItem(`chatMessageData${username}`) as string
+      );
+      const lstrgChatMessageDateArray = JSON.parse(
+        localStorage.getItem(`chatMessageDateArray${username}`) as string
+      );
+
+      setMessageData(lstrgChatMessageData);
+      setMessageDateArray(lstrgChatMessageDateArray);
+    }
+  }, []);
 
   return (
     <StyledEmptyChatBody className="scroll-box">
-      <StyledProfileGroupUIContainer>
-        <ProfileGroupUI username={username} />
-      </StyledProfileGroupUIContainer>
+      {ldsh.isEqual(messageData, {}) && messageDateArray.length === 0 && (
+        <StyledProfileGroupUIContainer>
+          <ProfileGroupUI username={username} />
+        </StyledProfileGroupUIContainer>
+      )}
     </StyledEmptyChatBody>
   );
 }
