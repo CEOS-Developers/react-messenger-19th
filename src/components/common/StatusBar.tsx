58325ea11bd519c4d40ac887cae8e8b499d238a0
CSE-pebble <CSE.pebble@gmail.com>
2024-05-03 03:21:39 +0900

    Modify: 상단바 현재 시간 반영

--- src/components/common/StatusBar.tsx
+++ src/components/common/StatusBar.tsx
@@ -1,11 +1,59 @@
-import statusBar from "../../assets/statusBar.svg";
+import celluar from "../../assets/celluar.svg";
+import wifi from "../../assets/wifi.svg";
+import battery from "../../assets/battery.svg";
+
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const StatusBar = () => {
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+
   return (
-    <>
-      <img src={statusBar} />
-    </>
+    <Wrapper>
+      <Time> {currentTime}</Time>
+      <Icon>
+        <img src={celluar} alt="celluar icon" />
+        <img src={wifi} alt="wifi icon" />
+        <img src={battery} alt="battery icon" />
+      </Icon>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.8rem 0.98rem 0.75rem 1.8rem;
+  justify-content: space-between;
+`;
+
+const Time = styled.span`
+  text-align: center;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.0175rem;
+  font-family: "SF Pro Text";
+`;
+
+const Icon = styled.div`
+  display: flex;
+  gap: 0.25rem;
+`;
 
 export default StatusBar;
