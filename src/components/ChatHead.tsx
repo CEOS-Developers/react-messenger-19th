1d3b67a1af2b78651f5e9be3811c2cc4ff504b30
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-03 23:07:16 +0900

    feat : 채팅방에서 -> 대화 페이지 가는 기능 만들기

--- src/components/ChatHead.tsx
+++ src/components/ChatHead.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { ChatHeadContainer, UserImage, UserName } from '../style/ChatHeadStyle';
 
 interface User {
-	name: string;
-	image: string;
+    name: string;
+    image: string;
 }
 
 interface ChatHeadProps {
-	user: User;
-	onUserClick: () => void;
+    user: User;
 }
 
-const ChatHead: React.FC<ChatHeadProps> = ({
-	user,
-	onUserClick = () => {},
-}) => {
-	return (
-		<ChatHeadContainer onClick={onUserClick}>
-			<UserImage
-				src={user.image}
-				alt='Profile'
-				style={{ width: '34px', height: '34px', borderRadius: '50%' }}
-			/>
-			<UserName>{user.name}</UserName>
-		</ChatHeadContainer>
-	);
+const ChatHead: React.FC<ChatHeadProps> = ({ user }) => {
+    const history = useHistory();
+
+    const handleUserClick = () => {
+        history.push('/chattinglist');
+    };
+
+    return (
+        <ChatHeadContainer onClick={handleUserClick}>
+            <UserImage
+                src={user.image}
+                alt='Profile'
+                style={{ width: '34px', height: '34px', borderRadius: '50%' }}
+            />
+            <UserName>{user.name}</UserName>
+        </ChatHeadContainer>
+    );
 };
 
 export default ChatHead;
