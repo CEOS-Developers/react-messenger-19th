140e98f050a99f446aabd0b713a5ae66cd6e4362
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-03 22:14:07 +0900

    feat : chattinglist 페이지

---
+++ src/pages/ChattingList.tsx
@@ -0,0 +1,49 @@
+// src/pages/ChattingList.tsx
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { chatListState } from '../state/chatListState'; // 대화 목록 상태
+import styled from 'styled-components';
+
+const ChatListContainer = styled.div`
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const ChatItem = styled(Link)`
+	width: 100%;
+	padding: 10px;
+	margin: 5px 0;
+	background-color: #f0f0f0;
+	color: black;
+	text-decoration: none;
+	border-radius: 5px;
+	white-space: pre-line; 
+
+	&:hover {
+		background-color: #e0e0e0;
+	}
+`;
+
+
+const ChattingList = () => {
+	const chatList = useRecoilValue(chatListState); // Recoil 상태에서 대화 목록을 가져옴
+
+	return (
+		<ChatListContainer>
+			{chatList.map((chat) => (
+			<ChatItem key={chat.id} to={`/chattingroom/${chat.id}`}>
+            {`${chat.partner}`} 
+            {'\n'}
+            ({chat.unreadCount} unread messages)
+          </ChatItem>
+          
+			))}
+		</ChatListContainer>
+	);
+};
+
+export default ChattingList;
