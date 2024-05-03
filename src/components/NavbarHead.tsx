2f5e5040193cfd0d9ea2796587c16fc941bf90c7
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-03 23:43:44 +0900

    .

--- src/components/NavbarHead.tsx
+++ src/components/NavbarHead.tsx
@@ -5,33 +5,24 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavbarHeadContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
-    position: fixed;
-    font-size:20px;
-    width: 375px;
-   top:-30px;
-   height: 56px;
-    background: #f0f0f0;
-    padding-top: 30px;
-	
-    
-   
+	display: flex;
+	justify-content: space-around;
+	position: fixed;
+	font-size: 20px;
+	width: 375px;
+	top: -30px;
+	height: 56px;
+	background: #f0f0f0;
+	padding-top: 30px;
 `;
 
 const NavbarHeadLink = styled(Link)`
-    text-decoration: none;
-    color: black;
-    
-    
+	text-decoration: none;
+	color: black;
 `;
 
 const NavbarHead = () => {
-    return (
-        <NavbarHeadContainer>
-       대화
-        </NavbarHeadContainer>
-    );
+	return <NavbarHeadContainer>대화</NavbarHeadContainer>;
 };
 
 export default NavbarHead;
