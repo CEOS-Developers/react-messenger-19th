0c3871db87eac973c1a8b77d65b77fdf41dca247
youdame <112458620+youdame@users.noreply.github.com>
2024-05-03 20:35:37 +0900

    Feat : contacts 페이지 완성

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -2,32 +2,94 @@ import SearchBar from 'components/common/SearchBar';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as PlusFriendIcon } from 'asset/icons/PlusFriendIcon.svg';
+import partnerList from 'data/userData.json';
+import { User } from 'types/ChatData';
+import { flexColumn } from 'styles/CommonStyle';
+import { getElapsedTime } from 'util/getElapsedTime';
 export default function Contacts() {
   const [inputValue, setInputValue] = useState('');
+
+  const [partners] = useState<User[]>(partnerList.users);
+
+  const filteredPartners = partners.filter((partners) => partners.name.includes(inputValue));
+
   return (
-    <>
+    <ContactsWrapper>
       <ContactsHeader>
+        <Spacer />
         <h1>친구 목록</h1>
         <button>
           <PlusFriendIcon alt="친구 추가 아이콘" />
         </button>
       </ContactsHeader>
       <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
-    </>
+
+      <PartnerList>
+        {filteredPartners.map((partner) => (
+          <Partner key={partner.id}>
+            <img src={partner.profileImage} />
+            <div>
+              <p className="name">{partner.name}</p>
+              <p className="last_access">
+                마지막 접속 &nbsp;
+                {partner?.lastAccess && getElapsedTime(partner?.lastAccess)}
+              </p>
+            </div>
+          </Partner>
+        ))}
+      </PartnerList>
+    </ContactsWrapper>
   );
 }
 
+const ContactsWrapper = styled.div`
+  ${flexColumn}
+  height: 68rem;
+`;
 const ContactsHeader = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
   padding: 1rem 1.6rem;
 
-  font-size: 1.7rem;
-  line-height: 2.2rem;
-  letter-spacing: -0.04rem;
-  justify-content: center;
-  align-items: center;
+  & h1 {
+    font-size: 1.7rem;
+    text-align: center; // 가운데 정렬
+  }
+`;
 
-  & button {
-    margin-left: auto;
+const Spacer = styled.div`
+  width: 3.2rem; // 버튼과 같은 너비로 설정하여 균형 유지
+`;
+
+const PartnerList = styled.div``;
+
+const Partner = styled.div`
+  width: 37.5rem;
+  height: 5.6rem;
+  flex-shrink: 0;
+  padding: 0.8rem 1.6rem;
+  background-color: var(--white);
+  display: flex;
+  gap: 1.3rem;
+  cursor: pointer;
+
+  & img {
+    width: 4rem;
+    height: 4rem;
+  }
+  .name {
+    color: var(--black);
+    font-size: 1.7rem;
+    line-height: 2.2rem; /* 129.412% */
+    letter-spacing: -0.04rem;
+  }
+
+  .last_access {
+    color: var(--gray03);
+    /* caption/caption */
+    font-size: 1.3rem;
+    letter-spacing: -0.01rem;
   }
 `;
