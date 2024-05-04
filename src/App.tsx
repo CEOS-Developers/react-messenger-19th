3b8a210b9c00ffd829dd4b491748d4cff55acfde
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-05-04 17:36:42 +0900

    feat: chathead는 app에서 관리되도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,29 @@ import ChattingList from './pages/ChattingList';
 import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
 import NavbarHead from './components/NavbarHead';
+import ChatHead from './components/ChatHead';
+import ChatBody from './components/ChatBody';
+import ChatBottom from './components/ChatBottom';
 
 const App = () => {
-    const { pathname } = useLocation();
-    const isChattingRoomPage = pathname.startsWith('/chattingroom');
+	const { pathname } = useLocation();
+	const isChattingRoomPage = pathname.startsWith('/chattingroom');
 
-    return (
-        <>
-            <NavbarHead />
-            <div style={{ paddingTop: '96px' }}>
-                <Routes>
-                    <Route path='/chattingroom/:userId' element={<ChattingRoom />} />
-                    <Route path='/userlist' element={<UserList />} />
-                    <Route path='/chattinglist' element={<ChattingList />} />
-                    <Route path='/profile' element={<Profile />} />
-                </Routes>
-            </div>
-            {!isChattingRoomPage && <Navbar />}
-        </>
-    );
+	return (
+		<>
+			{isChattingRoomPage ? <ChatHead user={{ name: '', image: '' }} /> : <NavbarHead />}
+			<div style={{ paddingTop: isChattingRoomPage ? '0px' : '80px' }}>
+				<Routes>
+					<Route path='/chattingroom/:userId' element={<ChattingRoom />} />
+					<Route path='/userlist' element={<UserList />} />
+					<Route path='/chattinglist' element={<ChattingList />} />
+					<Route path='/profile' element={<Profile />} />
+				</Routes>
+			</div>
+			{!isChattingRoomPage && <Navbar />}
+			
+		</>
+	);
 };
 
 export default App;
--- src/pages/ChattingRoom.tsx
+++ src/pages/ChattingRoom.tsx
@@ -8,64 +8,58 @@ import ChatBody from '../components/ChatBody';
 import ChatBottom from '../components/ChatBottom';
 
 interface Message {
-  id: number;
-  from: string;
-  to: string;
-  content: string;
-  date: string;
+	id: number;
+	from: string;
+	to: string;
+	content: string;
+	date: string;
 }
 
 const ChattingRoom = () => {
-  const { userId } = useParams<{ userId: string }>();
-  const messagesStateValue = useRecoilValue(messagesState);
-  const [messages, setMessages] = useState<Message[]>([]);
+	const { userId } = useParams<{ userId: string }>();
+	const messagesStateValue = useRecoilValue(messagesState);
+	const [messages, setMessages] = useState<Message[]>([]);
 
-  useEffect(() => {
-    // 로컬 스토리지에서 이전 메시지 가져오기
-    const storedMessages = localStorage.getItem(`messages_${userId}`);
-    if (storedMessages) {
-      // 이전 메시지가 있으면 파싱하여 상태에 설정
-      setMessages(JSON.parse(storedMessages));
-    }
-  }, [userId]); // userId가 변경될 때마다 이펙트 재실행
-  
+	useEffect(() => {
+		// 로컬 스토리지에서 이전 메시지 가져오기
+		const storedMessages = localStorage.getItem(`messages_${userId}`);
+		if (storedMessages) {
+			// 이전 메시지가 있으면 파싱하여 상태에 설정
+			setMessages(JSON.parse(storedMessages));
+		}
+	}, [userId]); // userId가 변경될 때마다 이펙트 재실행
 
-  const userList = useRecoilValue(userListState);
-  const currentUser = userList.find(user => user.id === userId);
+	const userList = useRecoilValue(userListState);
+	const currentUser = userList.find((user) => user.id === userId);
 
-  const sendMessage = (content: string, from: string, to: string) => {
-    const newMessage: Message = {
-      id: Date.now(),
-      from,
-      to,
-      content,
-      date: new Date().toISOString(),
-    };
-  
-    // 이전 메시지 배열과 새로운 메시지를 합침
-    const updatedMessages = [...messages, newMessage];
-    setMessages(updatedMessages);
-  
-    // localStorage에 업데이트된 메시지 저장
-    localStorage.setItem(`messages_${userId}`, JSON.stringify(updatedMessages));
-  };
-  
+	const sendMessage = (content: string, from: string, to: string) => {
+		const newMessage: Message = {
+			id: Date.now(),
+			from,
+			to,
+			content,
+			date: new Date().toISOString(),
+		};
 
-  return (
-    <div>
-      <ChatHead user={currentUser || { name: '', image: '' }} />
-      <ChatBody
-        messages={messages}
-        userImage={currentUser?.image ?? ''}
-        currentUser={currentUser?.name ?? ''}
-      />
-      <ChatBottom
-        onSendMessage={(content) =>
-          sendMessage(content, currentUser?.name ?? '', userId ?? '')
-        }
-      />
-    </div>
-  );
+		// 이전 메시지 배열과 새로운 메시지를 합침
+		const updatedMessages = [...messages, newMessage];
+		setMessages(updatedMessages);
+
+		// localStorage에 업데이트된 메시지 저장
+		localStorage.setItem(`messages_${userId}`, JSON.stringify(updatedMessages));
+	};
+
+	return (
+		<>
+			
+			<ChatBody messages={messages} userImage={currentUser?.image ?? ''} currentUser={currentUser?.name ?? ''} />
+			<ChatBottom
+				onSendMessage={(content) =>
+					sendMessage(content, currentUser?.name ?? '', userId ?? '')
+				}
+			/>
+		</>
+	);
 };
 
 export default ChattingRoom;
